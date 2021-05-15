20b9322b7fa68144f40c91d1c658c139ad87c7cb
Felipe Caetano <felipe.l.caetano@gmail.com>
2021-05-15 13:52:52 -0300

    chore: add initial setup with jest, eslint, prettier, npm and commitlint configs

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    extends: ['standard-with-typescript', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+}
